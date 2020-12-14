781eb8d8f3dc745200dd0a569bd9824b2dfce847
supermads <madisonsfahey@gmail.com>
2020-12-13 19:54:31 -0800

    Stage 3 of Project

--- Problems/Greetings/task.py
+++ Problems/Greetings/task.py
@@ -3,6 +3,13 @@ def main():
     print("I was created in 2020.")
     name = input("Please, remind me your name.\n")
     print("What a great name you have, {}!".format(name))
+    print("Let me guess your age.")
+    print("Enter remainders of dividing your age by 3, 5 and 7.")
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    age = (a * 70 + b * 21 + c * 15) % 105
+    print("Your age is {}; that's a good time to start programming!".format(age))
 
 
 main()
